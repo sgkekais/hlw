e8bf61d8b0cfb10763a9cb28c42efb0cf73b7fbd
Kevin <kvnkaiser@gmail.com>
2017-03-31 18:37:20 +0200

    created controllers

---
+++ app/Http/Controllers/Admin/MatchweekController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Matchweek;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class MatchweekController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Matchweek  $matchweek
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Matchweek $matchweek)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Matchweek  $matchweek
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Matchweek $matchweek)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Matchweek  $matchweek
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Matchweek $matchweek)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Matchweek  $matchweek
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Matchweek $matchweek)
+    {
+        //
+    }
+}
---
+++ app/Http/Controllers/Admin/RefereeController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Referee;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class RefereeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Referee  $referee
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Referee $referee)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Referee  $referee
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Referee $referee)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Referee  $referee
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Referee $referee)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Referee  $referee
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Referee $referee)
+    {
+        //
+    }
+}
