150464d87cd1557d3e4d8e6146a20033ac6fa8e3
Kevin <kvnkaiser@gmail.com>
2017-06-09 15:46:15 +0200

    updated model

--- app/Division.php
+++ app/Division.php
@@ -6,5 +6,32 @@ use Illuminate\Database\Eloquent\Model;
 
 class Division extends Model
 {
-    //
+    /**
+     * Mass assignable fields
+     */
+    protected $fillable = [
+        'name', 'hierachy_level'
+    ];
+
+    /**
+     * Relationship
+     * 1. Inverse One-to-Many
+     * - a division belongs to one competition
+     * - many divisions belong to one competition
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function competition(){
+        return $this->belongsTo(Competition::class);
+    }
+
+    /**
+     * Relationship
+     * 2. One-to-Many
+     * - a division has many seasons
+     * - (many) seasons belong to one division
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function seasons(){
+        return $this->hasMany(Season::class);
+    }
 }
