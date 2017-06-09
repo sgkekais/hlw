f3293b6f4d9324f8f4646cc055bd36668eeb7d45
Kevin <kvnkaiser@gmail.com>
2017-06-09 15:42:41 +0200

    updated model

--- app/Competition.php
+++ app/Competition.php
@@ -6,5 +6,22 @@ use Illuminate\Database\Eloquent\Model;
 
 class Competition extends Model
 {
-    //
+    /**
+     * Mass assignable fields
+     */
+    protected $fillable = [
+        'name'
+    ];
+
+    /**
+     * Relationships
+     * 1. One-to-Many
+     * - a competition has many divisions
+     * - many divisions belong to one competition
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function divisions(){
+        return $this->hasMany(Division::class);
+    }
+
 }
