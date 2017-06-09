5df9d83ab6b3c613a6917a2265b3172df186971e
Kevin <kvnkaiser@gmail.com>
2017-06-09 17:02:58 +0200

    updated model

--- app/Matchweek.php
+++ app/Matchweek.php
@@ -6,5 +6,30 @@ use Illuminate\Database\Eloquent\Model;
 
 class Matchweek extends Model
 {
-    //
+    // mass assignable fields
+    protected $fillable = [
+        'number_consecutive', 'name', 'begin', 'end'
+    ];
+
+    /**
+     * Relationship
+     * 1. One-to-Many
+     * - a matchweek belongs to one season
+     * - a season has many matchweeks
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function season(){
+        return $this->belongsTo(Season::class);
+    }
+
+    /**
+     * Relationship
+     * 2. One-to-Many
+     * - a matchweek has many fixtures
+     * - a fixture / match belongs to one matchweek
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function fixtures(){
+        return $this->hasMany(Fixture::class);
+    }
 }
