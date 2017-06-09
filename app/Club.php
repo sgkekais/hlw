bfc08cd6666a0a87befd0e32fd8c6cc72bc37533
Kevin <kvnkaiser@gmail.com>
2017-06-09 17:24:31 +0200

    updated models

--- app/Club.php
+++ app/Club.php
@@ -6,5 +6,33 @@ use Illuminate\Database\Eloquent\Model;
 
 class Club extends Model
 {
-    //
+    // mass assignable fields
+    protected $fillable = [
+        'name', 'name_short', 'name_code',
+        'logo_url',
+        'founded', 'league_entry', 'league_exit',
+        'colours_club', 'colours_kit',
+        'website', 'facebook',
+        'note'
+    ];
+
+    /**
+     * Relationship
+     * - a club has many players
+     * - a player belongs to one club at a given time
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function players(){
+        return $this->hasMany(Player::class);
+    }
+
+    public function seasons(){
+        return $this->belongsToMany(Season::class); // TODO: CHECK
+    }
+
+    public function stadiums(){
+        return $this->belongsToMany(Stadium::class); // TODO: CHECK
+    }
+
+    // TODO: more relationships, e.g. fixtures (home, away, all, create function in controller that uses relationship in combination with year)
 }
