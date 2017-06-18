010c51fa128d6ebcf3d30ea6d6c3611528a3686b
Kevin <kvnkaiser@gmail.com>
2017-06-18 13:46:35 +0200

    added log controller

---
+++ app/Http/Controllers/Admin/LogController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Spatie\Activitylog\Models\Activity;
+
+class LogController extends Controller
+{
+    public function index(){
+
+        $log = Activity::all();
+
+        return view('admin.log', compact('log'));
+    }
+}
