647889258db90154bf04585d20efc7eeb8e862ef
Kevin <kvnkaiser@gmail.com>
2017-06-20 14:43:42 +0200

    updated login view

--- resources/views/admin/competitions/create.blade.php
+++ resources/views/admin/competitions/create.blade.php
@@ -4,17 +4,18 @@
 
     <div class="container">
         <!-- create a new competition -->
-        <h1>Wettbewerb anlegen</h1>
+        <h1 class="mt-4">Wettbewerb anlegen</h1>
 
         <form method="POST" action="{{ route('competitions.store') }}">
             <!-- protection against CSRF (cross-site request forgery) attacks-->
             {{ csrf_field() }}
             <div class="form-group">
                 <label for="name">Name</label>
-                <input type="text" class="form-control" name="name" id="name" aria-describedby="nameHelp" placeholder="{{ old('name') }}">
+                <input type="text" class="form-control" name="name" id="name" aria-describedby="nameHelp" placeholder="{{ old('name', 'Hobbyliga-West') }}">
                 <small id="nameHelp" class="form-text text-muted">Bezeichnung des Wettbewerbs</small>
             </div>
             <button type="submit" class="btn btn-primary">Anlegen</button>
+            <a class="btn btn-secondary" href="{{ route('competitions.index') }}">Abbrechen</a>
         </form>
     </div>
 
