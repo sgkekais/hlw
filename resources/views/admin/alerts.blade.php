67966fdfa53a2799fe3cd00ef33d0d3141454efc
Kevin <kvnkaiser@gmail.com>
2017-06-20 15:41:40 +0200

    updated alerts

--- resources/views/admin/alerts.blade.php
+++ resources/views/admin/alerts.blade.php
@@ -1,9 +1,10 @@
 <!-- Info Alert -->
 @if (Session::has('info'))
-    <div class="alert alert-info alert-dismissible" role="alert">
-        <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+    <div class="alert alert-info alert-dismissible fade show mt-4" role="alert">
+        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+        </button>
         <span class="fa fa-info"></span> <strong>Info!</strong>
-        <hr>
         <p>
             {{ Session::get('info') }}
         </p>
@@ -11,10 +12,11 @@
 @endif
 <!-- Success Alert -->
 @if (Session::has('success'))
-    <div class="alert alert-success alert-dismissible" role="alert">
-        <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+    <div class="alert alert-success alert-dismissible fade show mt-4" role="alert">
+        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+        </button>
         <span class="fa fa-check"></span> <strong>Erfolg!</strong>
-        <hr>
         <p>
             {{ Session::get('success') }}
         </p>
@@ -22,10 +24,11 @@
 @endif
 <!-- Warning Alert -->
 @if (Session::has('warning'))
-    <div class="alert alert-warning alert-dismissible" role="alert">
-        <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+    <div class="alert alert-warning alert-dismissible fade show mt-4" role="alert">
+        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+        </button>
         <span class="fa fa-warning"></span> <strong>Warnung!</strong>
-        <hr>
         <p>
             {{ Session::get('warning') }}
         </p>
@@ -34,9 +37,10 @@
 <!-- Critical Alert -->
 @if (Session::has('danger'))
     <div class="alert alert-danger alert-dismissible" role="alert">
-        <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+        </button>
         <span class="fa fa-fire"></span> <strong>Achtung!</strong>
-        <hr>
         <p>
             {{ Session::get('danger') }}
         </p>
