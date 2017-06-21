7e63c0cf8a4859d4e0465aea6609488938867ff5
Kevin <kvnkaiser@gmail.com>
2017-06-22 00:18:04 +0200

    commented footer

--- resources/views/admin/_includes/footer.blade.php
+++ resources/views/admin/_includes/footer.blade.php
@@ -1,10 +1,10 @@
-<footer class="footer bg-faded">
+<!-- TODO <footer class="footer bg-faded">
     <div class="container">
         <div class="row justify-content-end no-gutters">
             <span class="text-muted">&copy; {{ date('Y') }} - Hobbyliga-West, Kevin Kaiser</span>
         </div>
     </div>
-</footer>
+</footer>-->
 
 <!-- Scripts -->
 <script src="{{ asset('js/app.js') }}"></script>
