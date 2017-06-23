b414e1b68915273d1834ae68d56359987e5a3e68
Kevin <kvnkaiser@gmail.com>
2017-06-23 15:32:20 +0200

    added season views

---
+++ resources/views/admin/seasons/create.blade.php
@@ -0,0 +1,58 @@
+@extends('admin.adminlayout')
+
+@section('content')
+
+    <div class="container">
+        <!-- create a new division -->
+        <h1 class="mt-4 mb-4">Spielklasse anlegen</h1>
+
+        <form method="POST" action="{{ route('divisions.store') }}">
+            <!-- protection against CSRF (cross-site request forgery) attacks-->
+            {{ csrf_field() }}
+            <div class="form-group row">
+                <div class="col-md-2">
+                    <label for="name">Name</label>
+                </div>
+                <div class="col-md-4">
+                    <input type="text" class="form-control" name="name" id="name" aria-describedby="nameHelp" placeholder="{{ old('name', 'Hobbyliga-West') }}">
+                    <small id="nameHelp" class="form-text text-muted">Bezeichnung der Spielklasse</small>
+                </div>
+            </div>
+            <!-- hierarchy level -->
+            <div class="form-group row">
+                <div class="col-md-2">
+                    <label for="hierarchy_level">Hierarchiebene</label>
+                </div>
+                <div class="col-md-4">
+                    <input type="number" class="form-control" name="hierarchy_level" id="hierarchy_level" aria-describedby="hierarchy_levelHelp" placeholder="{{ old('hierarchy_level', '1') }}">
+                    <small id="hierarchy_levelHelp" class="form-text text-muted">Hierarchiebene der Spielklasse, bspw. 2 für 2. Liga</small>
+                </div>
+            </div>
+            <!-- competition -->
+            <div class="form-group row">
+                <div class="col-md-2">
+                    <label for="competition_id">Wettbewerb</label>
+                </div>
+                <div class="col-md-4">
+                    <select class="form-control" id="competition_id" name="competition_id" aria-describedby="competition_idHelp">
+                        @foreach($competitions = \App\Competition::all() as $competition)
+                            <option value="{{ $competition->id }}">{{ $competition->name }}</option>
+                        @endforeach
+                    </select>
+                    <small id="competition_idHelp" class="form-text text-muted">Zuordnung zu welchem Wettbewerb?</small>
+                </div>
+            </div>
+            <!-- published -->
+            <div class="form-check">
+                <label class="form-check-label">
+                    <input type="checkbox" class="form-check-input" name="published" id="published" aria-describedby="pubishHelp">
+                    Veröffentlichen
+                    <small id="publishHelp" class="form-text text-muted">Spielklasse veröffentlichen?</small>
+                </label>
+            </div>
+            <button type="submit" class="btn btn-primary">Anlegen</button>
+            <a class="btn btn-secondary" href="{{ route('divisions.index') }}">Abbrechen</a>
+        </form>
+    </div>
+
+@endsection
---
+++ resources/views/admin/seasons/index.blade.php
@@ -0,0 +1,78 @@
+@extends('admin.adminlayout')
+
+@section('content')
+
+    <div class="container">
+        <h1 class="mt-4">Saisons</h1>
+        <p>
+            Verwaltung der Saisons.
+        </p>
+        <div class="row">
+            <div class="col-md-3">
+                <!-- controls -->
+                <a class="btn btn-primary" href="{{ route('seasons.create') }}" title="Saison anlegen">
+                    <span class="fa fa-plus-circle"></span> Saison anlegen
+                </a>
+            </div>
+        </div>
+        <hr>
+        <!-- list all seasons -->
+        <h2 class="mt-4">Angelegte Saisons <span class="badge badge-default">{{ $seasons->count() }}</span></h2>
+        <table class="table table-sm table-striped table-hover">
+            <thead class="thead-default">
+            <tr>
+                <th class="">ID</th>
+                <th class="">Name</th>
+                <th class="">Jahr</th>
+                <th class="">Veröffentlicht?</th>
+                <th class="">Aktionen</th>
+                <th class="">Änderungen</th>
+            </tr>
+            </thead>
+            <tbody>
+            @foreach($seasons as $season)
+                <tr>
+                    <td><b>{{ $season->id }}</b></td>
+                    <td>
+                        <a href="{{ route('seasons.show', $season ) }}" title="Anzeigen">{{ $season->name }}</a>
+                        <br>
+                        <span class="text-muted">{{ $season->division->name }}</span>
+                        <br>
+                        Spielwochen: {{ $season->matchweeks()->get()->count() }}
+                    </td>
+                    <td>
+                        @if($season->year_begin == $season->year_end)
+                            {{ $season->year_begin }}
+                        @else
+                            {{ $season->year_begin }} / {{ $season->year_end }}
+                    </td>
+                    <td>{{ $season->published ? "JA" : "NEIN" }}</td>
+                    <td>
+                        <!-- display details -->
+                        <a class="btn btn-secondary" href="{{ route('seasons.show', $season) }}" title="Saison anzeigen">
+                            <span class="fa fa-eye"></span>
+                        </a>
+                        <!-- edit -->
+                        <a class="btn btn-primary" href="{{ route('seasons.edit', $season) }}" title="Saison bearbeiten">
+                            <span class="fa fa-pencil-square-o" aria-hidden="true"></span>
+                        </a>
+                    </td>
+                    <td>
+                        angelegt am {{ $season->created_at->format('d.m.Y \\u\\m H:i') }} Uhr
+                        @if($causer = ModelHelper::causerOfAction($season,'created'))
+                            von {{ $causer->name }}
+                        @endif
+                        <br>
+                        @if($season->updated_at != $season->created_at)
+                            geändert am {{ $season->updated_at->format('d.m.Y \\u\\m H:i') }} Uhr
+                            @if($causer = ModelHelper::causerOfAction($season,'updated'))
+                                von {{ $causer->name }}
+                            @endif
+                        @endif
+                    </td>
+                </tr>
+            @endforeach
+            </tbody>
+        </table>
+    </div>
+@endsection
