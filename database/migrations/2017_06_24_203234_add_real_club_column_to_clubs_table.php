664bb1cb29ebfdad4bc516cb2a78a5b6c91833af
sgkekais <kvnkaiser@gmail.com>
2017-06-24 22:35:37 +0200

    added real club column

---
+++ database/migrations/2017_06_24_203234_add_real_club_column_to_clubs_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddRealClubColumnToClubsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('clubs', function (Blueprint $table) {
+            // add a "real club" column to indicate real clubs
+            $table->boolean('is_real_club')->default('0');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('clubs', function (Blueprint $table) {
+            //
+            $table->dropColumn('is_real_club');
+        });
+    }
+}
