e6a1ce5e1c6559f8c0d73a3b418e43755d313a2c
Kevin <kvnkaiser@gmail.com>
2017-06-29 12:04:18 +0200

    implemented model

--- app/Position.php
+++ app/Position.php
@@ -3,8 +3,26 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Spatie\Activitylog\Traits\LogsActivity;
 
 class Position extends Model
 {
-    //
+    use LogsActivity;
+
+    /**
+     * The attributes that should be logged.
+     * @var array
+     */
+    protected static $logAttributes = [
+        'name'
+    ];
+
+    /**
+     * The attributes that are mass assignable.
+     * @var array
+     */
+    protected $fillable = [
+        'name'
+    ];
+
 }
