48382819457a2d6ff4a28b92f3daa2e162db09a7
Kevin <kvnkaiser@gmail.com>
2017-06-29 17:26:15 +0200

    implemented show position

--- resources/views/admin/positions/show.blade.php
+++ resources/views/admin/positions/show.blade.php
@@ -3,13 +3,13 @@
 @section('content')
 
     <div class="container">
-        <h1 class="mt-4">Details zu Mannschaft</h1>
-        <h2 class="mt-4 text-primary">&mdash; {{ $club->name }}</h2>
+        <h1 class="mt-4">Details zu Position</h1>
+        <h2 class="mt-4 text-primary">&mdash; {{ $position->name }}</h2>
 
         <div class="row">
             <div class="col-md-6">
                 <h3 class="mt-4">Aktionen</h3>
-                <a class="btn btn-primary mb-4" href="{{ route('clubs.edit', $club ) }}" title="Wettbewerb bearbeiten">
+                <a class="btn btn-primary mb-4" href="{{ route('positions.edit', $position ) }}" title="Position bearbeiten">
                     <span class="fa fa-pencil"></span> Bearbeiten
                 </a>
             </div>
@@ -17,88 +17,60 @@
             <div class="col-md-6">
                 <h3 class="mt-4">Änderungen</h3>
                 <!-- published -->
-                @if($club->published)
+                @if($position->published)
                     <span class="fa fa-eye"></span> Öffentlich
                 @else
                     <span class="fa fa-eye-slash"></span> <b>Nicht</b> öffentlich
                 @endif
                 <br>
                 <!-- created at -->
-                Angelegt am: {{ $club->created_at->format('d.m.Y H:i') }} Uhr
-                @if($causer = ModelHelper::causerOfAction($club,'created'))
+                Angelegt am: {{ $position->created_at->format('d.m.Y H:i') }} Uhr
+                @if($causer = ModelHelper::causerOfAction($position,'created'))
                     von {{ $causer->name }}
                 @endif
                 <br>
                 <!-- updated at -->
-                @if($club->updated_at != $club->created_at)
-                    Geändert am: {{ $club->updated_at->format('d.m.Y H:i') }} Uhr
-                    @if($causer = ModelHelper::causerOfAction($club,'updated'))
+                @if($position->updated_at != $position->created_at)
+                    Geändert am: {{ $position->updated_at->format('d.m.Y H:i') }} Uhr
+                    @if($causer = ModelHelper::causerOfAction($position,'updated'))
                         von {{ $causer->name }}
                     @endif
                 @endif
             </div>
         </div>
         <hr>
-        <h3 class="mt-4 mb-4">Zuordnungen</h3>
-        <!-- show club tabs -->
-        <ul class="nav nav-tabs" role="tablist">
-            <li class="nav-item">
-                <a class="nav-link active" data-toggle="tab" href="#fixtures" role="tab">Paarungen</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" data-toggle="tab" href="#players" role="tab">Kader</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" data-toggle="tab" href="#stadiums" role="tab">Spielort(e)</a>
-            </li>
-        </ul>
-        <!-- show club details -->
-        <div class="tab-content">
-            <div class="tab-pane active" id="fixtures" role="tabpanel">
-                Saisons: {{ $club->seasons->count() }}
-
-                + jeweilige Paarungen
-            </div>
-            <div class="tab-pane" id="players" role="tabpanel">
-                <h4 class="mb-4 mt-4">Aktive  <span class="badge badge-default">{{ $players_active->count() }}</span></h4>
-                <a class="btn btn-primary mb-4" href="{{ route('players.create') }}" title="Spieler zuordnen">
-                    <span class="fa fa-pencil"></span> Spieler zuordnen
-                </a>
-                <table class="table table-sm table-striped table-hover">
-                    <thead class="thead-default">
+        <h3 class="mt-4 mb-4">Zugeordnete Spieler <span class="badge badge-default">{{ $position->players->count() }}</span></h3>
+            <table class="table table-sm table-striped table-hover">
+                <thead class="thead-default">
+                <tr>
+                    <th class="">ID</th>
+                    <th class="">Nachname, Vorname</th>
+                    <th class="">Mannschaft</th>
+                    <th class="">Aktionen</th>
+                    <th class="">Änderungen</th>
+                </tr>
+                </thead>
+                <tbody>
+                @foreach($position->players as $player)
                     <tr>
-                        <th class="">ID</th>
-                        <th class="">Nachname, Vorname</th>
-                        <th class="">Anmeldung</th>
-                        <th class="">Nummer</th>
-                        <th class="">Position</th>
-                        <th class="">Aktionen</th>
-                        <th class="">Änderungen</th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                    @foreach($players_active as $p_active)
-                        <tr>
-                            <td>{{ $p_active->id }}</td>
-                            <td>{{ $p_active->person->last_name }}, {{ $p_active->person->first_name }}</td>
-                            <td>{{ $p_active->sign_on->format('d.m.Y') }}</td>
-                            <td>{{ $p_active->number }}</td>
-                            <td>{{ $p_active->position_id }}</td>
-                            <td>
-                                <!-- edit -->
-                                <a class="btn btn-primary" href="{{ route('players.edit', $p_active) }}" title="Spieler bearbeiten">
+                        <td>{{ $player->id }}</td>
+                        <td>{{ $player->person->last_name }}, {{ $player->person->first_name }}</td>
+                        <td>{{ $player->club->name }}</td>
+                        <td>
+                            <!-- edit -->
+                                <a class="btn btn-primary" href="{{ route('players.edit', $player) }}" title="Spieler bearbeiten">
                                     <span class="fa fa-pencil-square-o" aria-hidden="true"></span>
                                 </a>
                             </td>
                             <td>
-                                angelegt am {{ $p_active->created_at->format('d.m.Y \\u\\m H:i') }} Uhr
-                                @if($causer = ModelHelper::causerOfAction($p_active,'created'))
+                                angelegt am {{ $player->created_at->format('d.m.Y \\u\\m H:i') }} Uhr
+                                @if($causer = ModelHelper::causerOfAction($player,'created'))
                                     von {{ $causer->name }}
                                 @endif
                                 <br>
-                                @if($p_active->updated_at != $p_active->created_at)
-                                    geändert am {{ $p_active->updated_at->format('d.m.Y \\u\\m H:i') }} Uhr
-                                    @if($causer = ModelHelper::causerOfAction($p_active,'updated'))
+                                @if($player->updated_at != $player->created_at)
+                                    geändert am {{ $player->updated_at->format('d.m.Y \\u\\m H:i') }} Uhr
+                                    @if($causer = ModelHelper::causerOfAction($player,'updated'))
                                         von {{ $causer->name }}
                                     @endif
                                 @endif
@@ -107,54 +79,6 @@
                     @endforeach
                     </tbody>
                 </table>
-                <hr>
-                <h4 class="mb-4 mt-4">Ehemalige  <span class="badge badge-default">{{ $club->players()->whereNotNull('sign_off')->count() }}</span></h4>
-                <table class="table table-sm table-striped table-hover">
-                    <thead class="thead-default">
-                    <tr>
-                        <th class="">ID</th>
-                        <th class="">Nachname, Vorname</th>
-                        <th class="">Anmeldung</th>
-                        <th class="">Abmeldung</th>
-                        <th class="">Aktionen</th>
-                        <th class="">Änderungen</th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                    @foreach($players_inactive as $p_inactive)
-                        <tr>
-                            <td>{{ $p_inactive->id }}</td>
-                            <td>{{ $p_inactive->person->last_name }}, {{ $p_inactive->person->first_name }}</td>
-                            <td>{{ $p_inactive->sign_on->format('d.m.Y') }}</td>
-                            <td>{{ $p_inactive->sign_off->format('d.m.Y') }}</td>
-                            <td>
-                                <!-- edit -->
-                                <a class="btn btn-primary" href="{{ route('players.edit', $p_inactive) }}" title="Spieler bearbeiten">
-                                    <span class="fa fa-pencil-square-o" aria-hidden="true"></span>
-                                </a>
-                            </td>
-                            <td>
-                                angelegt am {{ $p_inactive->created_at->format('d.m.Y \\u\\m H:i') }} Uhr
-                                @if($causer = ModelHelper::causerOfAction($p_inactive,'created'))
-                                    von {{ $causer->name }}
-                                @endif
-                                <br>
-                                @if($p_inactive->updated_at != $p_inactive->created_at)
-                                    geändert am {{ $p_inactive->updated_at->format('d.m.Y \\u\\m H:i') }} Uhr
-                                    @if($causer = ModelHelper::causerOfAction($p_inactive,'updated'))
-                                        von {{ $causer->name }}
-                                    @endif
-                                @endif
-                            </td>
-                        </tr>
-                    @endforeach
-                    </tbody>
-                </table>
-            </div>
-            <div class="tab-pane" id="stadiums" role="tabpanel">
-                Spielort(e): {{ $club->stadiums->count() }}
-
-            </div>
         </div>
     </div>
 @endsection
