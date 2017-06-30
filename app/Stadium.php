c24d1cef5c55a10e556e5617cdd31e0f4d25ec19
sgkekais <kvnkaiser@gmail.com>
2017-06-30 21:36:56 +0200

    updated many-to-many relationship with stadiums

--- app/Stadium.php
+++ app/Stadium.php
@@ -33,6 +33,8 @@ class Stadium extends Model
     ];
 
     public function clubs(){
-        return $this->belongsToMany(Club::class, 'clubs_stadiums'); // TODO: CHECK
+        return $this->belongsToMany(Club::class, 'clubs_stadiums')
+            ->withPivot('regular_home_day', 'regular_home_time', 'note', 'is_regular_stadium')
+            ->withTimestamps(); // TODO: CHECK, works with $club->stadiums()->save($stadium)!
     }
 }
