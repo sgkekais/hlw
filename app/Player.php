8c22d0a5ca68f395230b213c3bbcf5fd9f82c495
sgkekais <kvnkaiser@gmail.com>
2017-07-01 21:20:46 +0200

    added pivot model for clubs_people

--- app/Player.php
+++ app/Player.php
@@ -8,4 +8,32 @@ use Spatie\Activitylog\Traits\LogsActivity;
 class Player extends Pivot
 {
    use LogsActivity;
+
+    /**
+     * The attributes that should be logged.
+     * @var array
+     */
+    protected static $logAttributes = [
+        'sign_on', 'sign_off', 'number', 'position_id'
+    ];
+
+    /**
+     * The attributes that are mass assignable.
+     * @var array
+     */
+    protected $fillable = [
+        'sign_on', 'sign_off', 'number', 'position_id'
+    ];
+
+    /**
+     * The attributes that should be mutated to dates.
+     * @var array
+     */
+    protected $dates = [
+        'sign_on', 'sign_off'
+    ];
+
+    public function position(){
+        $this->belongsTo(Position::class);
+    }
 }
