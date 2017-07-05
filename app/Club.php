46c2a1be132adeb33d3fc9cc0c8b3fab6d2e782c
Kevin <kvnkaiser@gmail.com>
2017-07-05 22:09:29 +0200

    get fixtures method

--- app/Club.php
+++ app/Club.php
@@ -63,6 +63,20 @@ class Club extends Model
         return $query->where('is_real_club','1');
     }
 
+    /**
+     * Get all matches the club is related to
+     * TODO: add parameters to scope query, e.g. only matches of current season
+     * @return mixed
+     */
+    public function getFixtures()
+    {
+        $fixtures = Fixture::where('club_id_home', $this->id)
+            ->orWhere('club_id_away', $this->id)
+            ->get();
+
+        return $fixtures;
+    }
+
     /***********************************************************
      * RELATIONSHIPS
      ************************************************************/
@@ -72,7 +86,8 @@ class Club extends Model
      * A player can be related to many clubs (though not at the same time)
      * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
      */
-    public function players(){
+    public function players()
+    {
         return $this->belongsToMany(Person::class, 'clubs_people')
             ->withPivot('sign_on','sign_off','number','position_id')
             ->withTimestamps()
@@ -84,7 +99,8 @@ class Club extends Model
      * A season is related to many clubs
      * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
      */
-    public function seasons(){
+    public function seasons()
+    {
         return $this->belongsToMany(Season::class, 'clubs_seasons')
             ->withPivot('rank', 'deduction_points', 'deduction_goals', 'withdrawal', 'note')
             ->withTimestamps();
@@ -95,12 +111,28 @@ class Club extends Model
      * A stadium can be related to one or more clubs
      * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
      */
-    public function stadiums(){
+    public function stadiums()
+    {
         return $this->belongsToMany(Stadium::class, 'clubs_stadiums')
             ->withPivot('regular_home_day', 'regular_home_time', 'note', 'is_regular_stadium')
             ->withTimestamps(); // TODO: CHECK, works with $club->stadiums()->save($stadium)!
     }
 
-    // TODO: more relationships, e.g. fixtures (home, away, all, create function in controller that uses relationship in combination with year)
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function fixtures_home()
+    {
+        return $this->hasMany(Fixture::class, 'club_id_home');
+    }
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function fixtures_away()
+    {
+        return $this->hasMany(Fixture::class, 'club_id_away');
+    }
+
     // TODO: rescheduled by club
 }
