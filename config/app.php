aa57da8d77042176b70687623e764c5af1c4c439
Kevin <kvnkaiser@gmail.com>
2017-08-05 13:40:34 +0200

    added Carbon alias

--- config/app.php
+++ config/app.php
@@ -239,6 +239,8 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+        //
+        'Carbon' => Carbon\Carbon::class,
         // Helper Alias
         'ModelHelper' => HLW\Helpers\ModelHelper::class,
     ],
