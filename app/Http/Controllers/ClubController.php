961e2653f19b50d8367d566c66a2f4545df5606e
Kevin <kvnkaiser@gmail.com>
2017-08-15 17:52:51 +0200

    club controller for frontend

---
+++ app/Http/Controllers/ClubController.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace HLW\Http\Controllers;
+
+use HLW\Club;
+use HLW\Season;
+use Illuminate\Http\Request;
+
+class ClubController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Season $season = null)
+    {
+        $clubs = $season->clubs()->orderBy('name')->get();
+
+        return $clubs;
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \HLW\Club  $club
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Club $club)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \HLW\Club  $club
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Club $club)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \HLW\Club  $club
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Club $club)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \HLW\Club  $club
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Club $club)
+    {
+        //
+    }
+}
