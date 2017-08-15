509574e792ac811bf8bb6080694c6d0114e7e967
Kevin <kvnkaiser@gmail.com>
2017-08-15 20:25:15 +0200

    separated nav into partial

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -21,48 +21,11 @@
 </head>
 <body>
     <div id="app">
-            <nav class="navbar navbar-toggleable-md navbar-inverse ">
-                <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
 
-                <a class="navbar-brand" href="{{ route('home') }}">
-                    <img src="/images/hlwlogo_w.png" class="d-inline-block align-top" height="30" alt="HLW-Logo">
-                </a>
+        <!-- navigation -->
+        @include('_partials.nav')
 
-                <div class="collapse navbar-collapse" id="navbarCollapse">
-                    <ul class="navbar-nav mr-auto">
-                        <li class="nav-item" >
-                            <a class="nav-link active" href="/">Home</a>
-                        </li>
-                        @foreach(\HLW\Division::all() as $division)
-                            <li class="nav-item">
-                                <a class="nav-link" href="#">{{ $division->name }}</a>
-                            </li>
-                        @endforeach
-                    </ul>
-                    <ul class="navbar-nav">
-                        <li class="nav-item">
-                            <a class="nav-link active" href="#">Vorstand</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link active" href="">Satzung</a>
-                        </li>
-                    </ul>
-                </div>
-            </nav>
-            <ul class="nav">
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Tabelle & Paarungen</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="{{ route('frontend.seasons.clubs', $season) }}">Mannschaften</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Spieler</a>
-                </li>
-            </ul>
-
+        <!-- center content -->
         <div class="container">
             @yield('content')
         </div>
