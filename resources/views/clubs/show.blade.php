e18757f1e9c99925ada937062c6aaaffba99c65c
Kevin <kvnkaiser@gmail.com>
2017-08-18 11:06:39 +0200

    separated nav into partial

--- resources/views/clubs/show.blade.php
+++ resources/views/clubs/show.blade.php
@@ -5,13 +5,17 @@
 <div class="container-fluid">
     <!-- cover -->
     <div class="row mx-auto" style="width: 1140px;">
-        <div class="col-md-auto">
-            <img src="{{ Storage::url($club->logo_url) }}" title="{{ $club->name }}" alt="Vereinswappen">
+        <div class="col-md-auto" style="min-width: 200px">
+            @if($club->logo_url)
+                <img src="{{ Storage::url($club->logo_url) }}" title="{{ $club->name }}" alt="Vereinswappen">
+            @else
+                <span class="fa fa-ban text-muted fa-5x"></span>
+            @endif
         </div>
         <div class="col-md-8">
             <h1>{{ $club->name }}</h1>
-            <ul>
-                <li>{{ $club->regularStadium()->first()->name }}</li>
+            <ul class="list-unstyled">
+                <li>{{ $club->regularStadium()->first() ? $club->regularStadium()->first()->name : "Kein Stadion" }}</li>
             </ul>
         </div>
     </div>
