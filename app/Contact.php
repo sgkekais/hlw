bf227b5726ae86d09ede59acec44719c1ab60b25
Kevin <kvnkaiser@gmail.com>
2017-11-04 12:41:39 +0100

    added annotations

--- app/Contact.php
+++ app/Contact.php
@@ -5,6 +5,30 @@ namespace HLW;
 use Illuminate\Database\Eloquent\Model;
 use Spatie\Activitylog\Traits\LogsActivity;
 
+/**
+ * HLW\Contact
+ *
+ * @property int $id
+ * @property int $club_id
+ * @property int $person_id
+ * @property int|null $hierarchy_level
+ * @property string|null $mail
+ * @property string|null $mobile
+ * @property \Carbon\Carbon|null $created_at
+ * @property \Carbon\Carbon|null $updated_at
+ * @property-read \Illuminate\Database\Eloquent\Collection|\Spatie\Activitylog\Models\Activity[] $activity
+ * @property-read \HLW\Club $club
+ * @property-read \HLW\Person $person
+ * @method static \Illuminate\Database\Eloquent\Builder|\HLW\Contact whereClubId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\HLW\Contact whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\HLW\Contact whereHierarchyLevel($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\HLW\Contact whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\HLW\Contact whereMail($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\HLW\Contact whereMobile($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\HLW\Contact wherePersonId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\HLW\Contact whereUpdatedAt($value)
+ */
+
 class Contact extends Model
 {
     use LogsActivity;
@@ -30,6 +54,18 @@ class Contact extends Model
     protected $fillable = [
         'club_id', 'person_id', 'hierarchy_level', 'mail', 'mobile'
     ];
+
+    /***********************************************************
+     * SCOPES
+     ************************************************************/
+
+    /***********************************************************
+     * FUNCTIONS
+     ************************************************************/
+
+    /***********************************************************
+     * RELATIONSHIPS
+     ************************************************************/
 
     /**
      *
