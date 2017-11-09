6a6aa0017eb726f4e62af11effe23179478b559d
Kevin <kvnkaiser@gmail.com>
2017-11-09 21:43:03 +0100

    added fixtures in current week

--- app/Http/Controllers/PortalController.php
+++ app/Http/Controllers/PortalController.php
@@ -2,21 +2,30 @@
 
 namespace HLW\Http\Controllers;
 
+use Carbon\Carbon;
 use HLW\Competition;
 use HLW\Division;
+use HLW\Fixture;
 use HLW\Matchweek;
 use HLW\Season;
 use Illuminate\Http\Request;
+use Forecast\Forecast;
 
 class PortalController extends Controller
 {
     public function index()
     {
+
         $divisions = Division::published()->get()->where('competition.type', 'league');
         $divisions->load(['seasons' => function ($query) {
             $query->published()->current();
         }], 'seasons.clubs', 'seasons.matchweeks');
 
-        return view('index', compact('divisions'));
+        // get the fixtures of the current week
+        $monday = Carbon::now()->startOfWeek();
+        $sunday = Carbon::now()->endOfWeek();
+        $fixtures = Fixture::whereBetween('datetime', [$monday, $sunday])->orderBy('datetime')->get();
+
+        return view('index', compact('divisions', 'fixtures'));
     }
 }
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,25 +1,100 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class=" jumbotron jumbotron-fluid">
-        <div class="display-1 font-italic" style="color: #ff9800">HEADER</div>
+    <div class="jumbotron jumbotron-fluid" style="color: yellow; background: url('/images/duesseldorf.jpg'); background-size: cover">
+        <div class="container">
+            <div class="col-10 p-0">
+                <div class="display-4 font-weight-bold">
+                    Hobbyliga-West<br>
+                    Düsseldorf
+                </div>
+                <div>
+
+                </div>
+            </div>
+            <div class="col-2">
+
+            </div>
+        </div>
     </div>
     <div class="container">
 
         <div class="row">
             <div class="col-12">
-                News?
+                <h3 class="font-weight-bold">News</h3>
             </div>
         </div>
-        <div class="row">
-            <div class="col-12">
-                Aktuelle Paarungen über alle Ligen?
+        <hr>
+        <!-- fixtures of the current week -->
+        @if($fixtures)
+            <div class="row">
+                <div class="col">
+                    <h3 class="font-weight-bold">In dieser Woche</h3>
+                </div>
             </div>
-        </div>
+            <div class="row mt-4 mb-4">
+                @foreach ($fixtures as $fixture)
+                    <div class="col {{ !$loop->last ? "border border-left-0 border-top-0 border-bottom-0" : null }}">
+                        {{-- details --}}
+                        <div class="row">
+                            <div class="col-6 pr-0 text-muted">
+                                <small>{{ $fixture->datetime ? $fixture->datetime->format('d.m.') : "-" }}</small>
+                            </div>
+                            <div class="col-6 pl-0 text-muted text-right">
+                                <small>{{ $fixture->datetime ? $fixture->datetime->format('H:i') : "-" }}</small>
+                            </div>
+                        </div>
+                        {{-- top row --}}
+                        <div class="row">
+                            <div class="col-8 pr-0">
+                                @if($fixture->clubHome->logo_url)
+                                    <img src="{{ Storage::url($fixture->clubHome->logo_url) }}" width="30" class="pr-1">
+                                @else
+                                    <span class="fa fa-ban text-muted" title="Kein Vereinswappen vorhanden"></span>
+                                @endif
+                                {{ $fixture->clubHome ? $fixture->clubHome->name_code : "-" }}
+                            </div>
+                            <div class="col pl-0 text-right">
+                                @if($fixture->isPlayed() && !$fixture->isRated())
+                                    {{ $fixture->goals_home ?? "-" }}
+                                @elseif($fixture->isRated())
+                                    {{ $fixture->goals_home_rated ?? "-" }}
+                                @else
+                                    -
+                                @endif
+                            </div>
+                        </div>
+                        {{-- bottom row --}}
+                        <div class="row">
+                            <div class="col-8 pr-0">
+                                @if($fixture->clubAway->logo_url)
+                                    <img src="{{ Storage::url($fixture->clubAway->logo_url) }}" width="30" class="pr-1">
+                                @else
+                                    <span class="fa fa-ban text-muted" title="Kein Vereinswappen vorhanden"></span>
+                                @endif
+                                {{ $fixture->clubAway ? $fixture->clubAway->name_code : "-" }}
+                            </div>
+                            <div class="col pl-0 text-right">
+                                @if($fixture->isPlayed() && !$fixture->isRated())
+                                    {{ $fixture->goals_away ?? "-" }}
+                                @elseif($fixture->isRated())
+                                    {{ $fixture->goals_away_rated ?? "-" }}
+                                @else
+                                    -
+                                @endif
+                            </div>
+                        </div>
+                    </div>
+                @endforeach
+            </div>
+        @endif
+        <!-- end fixtures of the current week -->
+        <!-- shortened standings for leagues -->
+        <hr>
         <div class="row">
             @foreach ($divisions as $division)
                 <div class="col-md-4">
-                    <h3 class="font-weight-bold">{{ $division->name }}</h3>
+                    <h3 class="font-weight-bold">{{ $division->competition->name_short." | ".$division->name }}</h3>
                     @php
                         // $c_season = $division->seasons()->published()->current()->first();
                         $c_season = $division->currentSeason();
@@ -28,6 +103,7 @@
                         }
                     @endphp
                     @if ($c_season && $c_matchweek)
+                        {{-- TODO: render partial view for this collection ('c_season') --}}
                         <h4 class="text-muted">
                             SW {{ $c_matchweek->number_consecutive ?? null }}
                             <small class="text-muted">
@@ -64,7 +140,7 @@
                                         @endphp
                                     @elseif(in_array($club->t_rank, $c_season->playoff_relegation))
                                         @php
-                                            $rank_color = "#F44336";
+                                            $rank_color = "#FF9800";
                                             $rank_icon  = "fa-circle-o";
                                         @endphp
                                     @endif
@@ -94,8 +170,8 @@
                         </table>
                         <div class="text-right">
                             <ul class="list-inline">
-                                <li class="list-inline-item"><a href="{{ route('frontend.divisions.fixtures', $division) }}">Spielplan</a></li>
-                                <li class="list-inline-item"><a href="{{ route('frontend.divisions.tables', $division) }}">Tabelle</a></li>
+                                <li class="list-inline-item"><span class="fa fa-fw fa-calendar-o"></span> <a href="{{ route('frontend.divisions.fixtures', $division) }}">Spielplan</a></li>
+                                <li class="list-inline-item"><span class="fa fa-fw fa-table"></span> <a href="{{ route('frontend.divisions.tables', $division) }}">Tabelle</a></li>
                             </ul>
                         </div>
                     @endif
