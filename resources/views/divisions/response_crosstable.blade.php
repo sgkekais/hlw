f41099898db1d804e2f87d047c6e70b1ffdd6b5a
Kevin <kvnkaiser@gmail.com>
2017-12-07 00:12:26 +0100

    text fixes

--- resources/views/divisions/response_crosstable.blade.php
+++ resources/views/divisions/response_crosstable.blade.php
@@ -2,7 +2,7 @@
 <table class="table table-striped table-responsive-md">
     <thead>
         <tr>
-            <th class="text-left">Verein</th>
+            <th class="text-left">&nbsp;</th>
             @foreach($season->clubs->sortBy('name') as $club)
                 <th class="text-center">
                     @if($club->logo_url)
@@ -19,14 +19,14 @@
             <tr>
                 <td class="align-middle text-left">
                     @if($club_a->logo_url)
-                        <a href="{{ route('frontend.clubs.show', $club_a->id) }}" title="Zur Teamseite"><img src="{{ asset('storage/'.$club->logo_url) }}" width="30" title="{{ $club_a->name }}"></a>
+                        <a href="{{ route('frontend.clubs.show', $club_a->id) }}" title="Zur Teamseite"><img src="{{ asset('storage/'.$club_a->logo_url) }}" width="30" title="{{ $club_a->name }}"></a>
                     @else
                         <span class="fa fa-fw fa-ban text-secondary"></span>
                     @endif
                 </td>
                 @foreach($season->clubs->sortBy('name') as $club_b)
                     @if($club_b->id == $club_a->id)
-                        <td class="align-middle text-center">&nbsp;</td>
+                        <td class="align-middle text-center">=</td>
                     @else
                         <td class="align-middle text-center">
                             @php
@@ -34,14 +34,14 @@
                             @endphp
                             @if($fixture)
                                 @if($fixture->isPlayed() && !$fixture->isRated())
-                                    <span class="badge badge-pill {{ $club_a->hasWon($fixture) ? "badge-success" : null  }} {{ $club_a->hasDrawn($fixture) ? "badge-secondary" : null }} {{ $club_a->hasLost($fixture) ? "badge-danger" : null }}" style="font-size: 100%">{{ $fixture->goals_home }}:{{ $fixture->goals_away }}</span>
+                                    <span class="badge badge-pill font-weight-normal {{ $club_a->hasWon($fixture) ? "badge-success" : null  }} {{ $club_a->hasDrawn($fixture) ? "badge-secondary" : null }} {{ $club_a->hasLost($fixture) ? "badge-danger" : null }}" style="font-size: 100%">{{ $fixture->goals_home }}:{{ $fixture->goals_away }}</span>
                                 @elseif($fixture->isRated())
-                                    <span class="badge badge-pill badge-warning" style="font-size: 100%">{{ $fixture->goals_home_rated }}:{{ $fixture->goals_away_rated }}</span>
+                                    <span class="badge badge-pill font-weight-normal badge-warning" style="font-size: 100%">{{ $fixture->goals_home_rated }}:{{ $fixture->goals_away_rated }}</span>
                                 @else
-                                    <span class="badge badge-pill" style="font-size: 100%">-:-</span>
+                                    <span class="badge badge-pill font-weight-normal" style="font-size: 100%">-:-</span>
                                 @endif
                             @else
-                                <span class="badge badge-pill" style="font-size: 100%">-:-</span>
+                                <span class="badge badge-pill font-weight-normal" style="font-size: 100%">-:-</span>
                             @endif
                         </td>
                     @endif
