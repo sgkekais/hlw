798fe88d2879766016e590f0b0b17a3908780af5
Kevin <kvnkaiser@gmail.com>
2017-12-09 13:58:10 +0100

    name min length

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -61,7 +61,7 @@ class RegisterController extends Controller
     protected function validator(array $data)
     {
         return Validator::make($data, [
-            'name'      => 'required|string|max:255',
+            'name'      => 'required|string|min:2|max:20|unique:users',
             'email'     => 'required|string|email|max:255|unique:users',
             'password'  => 'required|string|min:6|confirmed',
         ]);
