e3dd1886086861ea4f2051e58cb70489b041ae0f
Kevin <kvnkaiser@gmail.com>
2018-01-20 13:29:05 +0100

    translation

--- resources/views/vendor/notifications/email.blade.php
+++ resources/views/vendor/notifications/email.blade.php
@@ -4,9 +4,9 @@
 # {{ $greeting }}
 @else
 @if ($level == 'error')
-# Whoops!
+# Ups!
 @else
-# Hello!
+# Hi!
 @endif
 @endif
 
@@ -45,14 +45,14 @@
 @if (! empty($salutation))
 {{ $salutation }}
 @else
-Regards,<br>{{ config('app.name') }}
+Grüße,<br>{{ config('app.name') }}
 @endif
 
 {{-- Subcopy --}}
 @isset($actionText)
 @component('mail::subcopy')
-If you’re having trouble clicking the "{{ $actionText }}" button, copy and paste the URL below
-into your web browser: [{{ $actionUrl }}]({{ $actionUrl }})
+Wenn du Probleme hast, den "{{ $actionText }}" Button zu drücken, dann kopiere die unten angegebene URL und füge sie in deinem
+Webbrowser ein: [{{ $actionUrl }}]({{ $actionUrl }})
 @endcomponent
 @endisset
 @endcomponent
