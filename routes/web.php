a5f3acf6aa308957a05da1e7d1e5ec1c992793b3
Kevin <kvnkaiser@gmail.com>
2018-01-20 18:37:06 +0100

    implementing user verification

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,9 @@
  ******************************************************/
 
 Auth::routes();
+// user verification
+Route::get('email-verification/error', 'Auth\RegisterController@getVerificationError')->name('email-verification.error');
+Route::get('email-verification/check/{token}', 'Auth\RegisterController@getVerification')->name('email-verification.check');
 
 Route::get('/', 'PagesController@index')->name('home');
 
