cd76f66e8b9ef29e5fcee79c917879397dd480a2
Kevin <kvnkaiser@gmail.com>
2018-01-25 20:25:07 +0100

    add display name to roles table

---
+++ database/migrations/2018_01_25_200650_add_display_name_column_to_roles_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddDisplayNameColumnToRolesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('roles', function (Blueprint $table) {
+            //
+            $table->string('display_name')->nullable()->after('name');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('roles', function (Blueprint $table) {
+            //
+            $table->dropColumn('display_name');
+        });
+    }
+}
