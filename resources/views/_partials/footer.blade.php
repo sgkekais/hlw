38283a70e7cbcce925053c15289d4f4ae97ce213
Kevin <kvnkaiser@gmail.com>
2018-01-30 22:29:39 +0100

    added link to bsgcup

--- resources/views/_partials/footer.blade.php
+++ resources/views/_partials/footer.blade.php
@@ -33,15 +33,20 @@
                     <li><span class="fa fa-fw fa-twitter"></span> <a href="https://twitter.com/HobbyligaWest" title="Twitter" target="_blank">HLW auf Twitter</a> <span class="fa fa-external-link"></span></li>
                     <li><span class="fa fa-fw fa-envelope"></span> vorstand [AT] hobbyligawest [DOT] de</li>
                 </ul>
-                @guest
-                    <a href="{{ route('login') }}" title="Anmelden">Login</a> |
-                    <a href="{{ route('register') }}" title="Registrieren">Registrieren</a>
-                @endguest
-                @auth
-                    <a href="{{ route('frontend.user.profile.show') }}" title="Profil">Profil</a> |
-                    <a href="{{ route('logout') }}" title="Abmelden">Logout</a>
-                @endauth
-
+                <h5 class="font-weight-bold">Freunde</h5>
+                <ul class="list-unstyled">
+                    <li><img src="http://www.bsgdbuv-cup.de/files/bsg-liga-logo-original-png.png" class="bg-white rounded" height="50"> <a href="http://www.bsgdbuv-cup.de/" target="_blank" class="pl-2" title="BSG-Liga">BSG-Cup Düsseldorf</a> <span class="fa fa-fw fa-external-link"></span></li>
+                </ul>
+                <span class="pull-right">
+                    @guest
+                        <a href="{{ route('login') }}" title="Anmelden">Login</a> |
+                        <a href="{{ route('register') }}" title="Registrieren">Registrieren</a>
+                    @endguest
+                    @auth
+                        <a href="{{ route('frontend.user.profile.show') }}" title="Profil">Profil</a> |
+                        <a href="{{ route('logout') }}" title="Abmelden">Logout</a>
+                    @endauth
+                </span>
             </div>
         </div>
         <div class="row pb-2">
