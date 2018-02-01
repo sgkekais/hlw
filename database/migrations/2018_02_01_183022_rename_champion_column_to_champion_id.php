407532591551d0ff4a623e8e53f7e5cc730223f1
Kevin <kvnkaiser@gmail.com>
2018-02-01 18:44:33 +0100

    fix contact and referee layout, fix champion assignment for seasons

---
+++ database/migrations/2018_02_01_183022_rename_champion_column_to_champion_id.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class RenameChampionColumnToChampionId extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('seasons', function (Blueprint $table) {
+            $table->renameColumn('champion', 'champion_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('seasons', function (Blueprint $table) {
+            $table->renameColumn('champion_id', 'champion');
+        });
+    }
+}
