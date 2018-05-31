80faeea0eb233c3ae4f47bfc8a25d00bafa63161
Kevin <kvnkaiser@gmail.com>
2018-05-31 22:23:46 +0200

    Datenschutz

--- resources/views/static/datenschutz.blade.php
+++ resources/views/static/datenschutz.blade.php
@@ -16,6 +16,25 @@
         </div>
         <div class="row">
             <div class="col">
+                <h2 class="font-weight-bold font-italic">Inhalt</h2>
+                <ol>
+                    <li><a href="#grundlegendes">Grundlegendes</a></li>
+                    <li><a href="#zugriffsdaten">Zugriffsdaten</a></li>
+                    <li><a href="#cookies">Cookies</a></li>
+                    <li><a href="#ssl">SSL-Verschlüsselung</a></li>
+                    <li><a href="#skriptbibliotheken">Verwendung von Skriptbibliotheken</a></li>
+                    <li><a href="#youtube">Eingebettete YouTube-Videos</a></li>
+                    <li><a href="#kontaktdaten">Umgang mit Kontaktdaten</a></li>
+                    <li><a href="#kommentare">Umgang mit Kommentaren und Beiträgen</a></li>
+                    <li><a href="#rechte">Rechte des Nutzers</a></li>
+                    <li><a href="#loeschung">Löschung von Daten</a></li>
+                </ol>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col">
+                <a id="grundlegendes"></a>
                 <h2 class="font-weight-bold font-italic">Grundlegendes</h2>
                 <p>
                     Diese Datenschutzerklärung soll die Nutzer dieser Website über die Art, den Umfang und den Zweck der Erhebung und Verwendung personenbezogener Daten durch den Websitebetreiber [<a href="mailto:webmaster@hobbyligawest.de" title="Mail an Webmaster">webmaster</a>] informieren.
@@ -30,6 +49,7 @@
         </div>
         <div class="row">
             <div class="col">
+                <a id="zugriffsdaten"></a>
                 <h2 class="font-weight-bold font-italic">Zugriffsdaten</h2>
                 <p>
                     Wir, der Websitebetreiber bzw. Seitenprovider, erheben aufgrund unseres berechtigten Interesses (s. Art. 6 Abs. 1 lit. f. DSGVO) Daten über Zugriffe auf die Website und speichern diese als „Server-Logfiles“ auf dem Server der Website ab. Folgende Daten werden so protokolliert:
@@ -46,6 +66,7 @@
         </div>
         <div class="row">
             <div class="col">
+                <a id="cookies"></a>
                 <h2 class="font-weight-bold font-italic">Cookies</h2>
                 <p>
                     Wie viele andere Webseiten verwenden wir auch so genannte „Cookies“. Cookies sind kleine Textdateien, die von einem Websiteserver auf Ihre Festplatte übertragen werden.
@@ -63,6 +84,7 @@
         </div>
         <div class="row">
             <div class="col">
+                <a id="ssl"></a>
                 <h2 class="font-weight-bold font-italic">SSL-Verschlüsselung</h2>
                 <p>
                     Um die Sicherheit Ihrer Daten bei der Übertragung zu schützen, verwenden wir dem aktuellen Stand der Technik entsprechende Verschlüsselungsverfahren (z. B. SSL) über HTTPS.
@@ -71,6 +93,7 @@
         </div>
         <div class="row">
             <div class="col">
+                <a id="skriptbibliotheken"></a>
                 <h2 class="font-weight-bold font-italic">Verwendung von Skriptbibliotheken (Google Webfonts)</h2>
                 <p>
                     Um unsere Inhalte browserübergreifend korrekt und grafisch ansprechend darzustellen, verwenden wir auf dieser Website Scriptbibliotheken und Schriftbibliotheken wie z. B. Google Webfonts (https://www.google.com/webfonts/). Google Webfonts werden zur Vermeidung mehrfachen Ladens in den Cache Ihres Browsers übertragen. Falls der Browser die Google Webfonts nicht unterstützt oder den Zugriff unterbindet, werden Inhalte in einer Standardschrift angezeigt.
@@ -83,6 +106,7 @@
         </div>
         <div class="row">
             <div class="col">
+                <a id="youtube"></a>
                 <h2 class="font-weight-bold font-italic">Eingebettete YouTube-Videos</h2>
                 <p>
                    Im Clubhaus betten wir Youtube-Videos ein. Betreiber der entsprechenden Plugins ist die YouTube, LLC, 901 Cherry Ave., San Bruno, CA 94066, USA. Wenn Sie eine Seite mit dem YouTube-Plugin besuchen, wird eine Verbindung zu Servern von Youtube hergestellt. Dabei wird Youtube mitgeteilt, welche Seiten Sie besuchen. Wenn Sie in Ihrem Youtube-Account eingeloggt sind, kann Youtube Ihr Surfverhalten Ihnen persönlich zuzuordnen. Dies verhindern Sie, indem Sie sich vorher aus Ihrem Youtube-Account ausloggen.
@@ -98,9 +122,9 @@
                 </p>
             </div>
         </div>
-        <i
         <div class="row">
             <div class="col">
+                <a id="kontaktdaten"></a>
                 <h2 class="font-weight-bold font-italic">Umgang mit Kontaktdaten</h2>
                 <p>
                     Nehmen Sie mit uns als Websitebetreiber durch die angebotenen Kontaktmöglichkeiten Verbindung auf, werden Ihre Angaben gespeichert, damit auf diese zur Bearbeitung und Beantwortung Ihrer Anfrage zurückgegriffen werden kann. Ohne Ihre Einwilligung werden diese Daten nicht an Dritte weitergegeben.
@@ -109,6 +133,7 @@
         </div>
         <div class="row">
             <div class="col">
+                <a id="kommentare"></a>
                 <h2 class="font-weight-bold font-italic">Umgang mit Kommentaren und Beiträgen</h2>
                 <p>
                     Hinterlassen Sie auf dieser Website einen Beitrag oder Kommentar, wird Ihre IP-Adresse gespeichert. Dies erfolgt aufgrund unserer berechtigten Interessen im Sinne des Art. 6 Abs. 1 lit. f. DSGVO und dient der Sicherheit von uns als Websitebetreiber: Denn sollte Ihr Kommentar gegen geltendes Recht verstoßen, können wir dafür belangt werden, weshalb wir ein Interesse an der Identität des Kommentar- bzw. Beitragsautors haben.
@@ -117,6 +142,7 @@
         </div>
         <div class="row">
             <div class="col">
+                <a id="rechte"></a>
                 <h2 class="font-weight-bold font-italic">Rechte des Nutzers</h2>
                 <p>
                     Sie haben als Nutzer das Recht, auf Antrag eine kostenlose Auskunft darüber zu erhalten, welche personenbezogenen Daten über Sie gespeichert wurden. Sie haben außerdem das Recht auf Berichtigung falscher Daten und auf die Verarbeitungseinschränkung oder Löschung Ihrer personenbezogenen Daten. Falls zutreffend, können Sie auch Ihr Recht auf Datenportabilität geltend machen. Kontaktieren Sie dazu den Webmaster.
@@ -128,6 +154,7 @@
         </div>
         <div class="row">
             <div class="col">
+                <a id="loeschung"></a>
                 <h2 class="font-weight-bold font-italic">Löschung von Daten</h2>
                 <p>
                     Sofern Ihr Wunsch nicht mit einer gesetzlichen Pflicht zur Aufbewahrung von Daten (z. B. Vorratsdatenspeicherung) kollidiert, haben Sie ein Anrecht auf Löschung Ihrer Daten. Von uns gespeicherte Daten werden, sollten sie für ihre Zweckbestimmung nicht mehr vonnöten sein und es keine gesetzlichen Aufbewahrungsfristen geben, gelöscht. Falls eine Löschung nicht durchgeführt werden kann, da die Daten für zulässige gesetzliche Zwecke erforderlich sind, erfolgt eine Einschränkung der Datenverarbeitung. In diesem Fall werden die Daten gesperrt und nicht für andere Zwecke verarbeitet.
