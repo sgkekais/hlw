46f05de4a4095ffa32d708c5c8f76459eff182d4
Kevin <kvnkaiser@gmail.com>
2018-10-07 21:49:01 +0200

    fix call of todatestring on null

--- resources/views/admin/fixtures/edit.blade.php
+++ resources/views/admin/fixtures/edit.blade.php
@@ -70,7 +70,7 @@
                 <label for="matchweek_id">Spielwoche</label>
             </div>
             <div class="col-md-4">
-                <input type="text" class="form-control" name="matchweek_id" aria-describedby="matchweek_idHelp" value="({{ $matchweek->id }}) Nr. {{ $matchweek->number_consecutive }} | {{ $matchweek->begin->format('d.m.Y') }} - {{ $matchweek->end->format('d.m.Y') }}" disabled>
+                <input type="text" class="form-control" name="matchweek_id" aria-describedby="matchweek_idHelp" value="({{ $matchweek->id }}) Nr. {{ $matchweek->number_consecutive }} | {{ $matchweek->begin ? $matchweek->begin->format('d.m.Y') : null }} - {{ $matchweek->end ? $matchweek->end->format('d.m.Y') : null }}" disabled>
                 <small id="matchweek_idHelp" class="form-text text-muted">Zuordnung zu welcher Spielwoche?</small>
             </div>
         </div>
