aee2847f35e9f568a35f9d6bbb596422e2be24e2
kvnka <kvnkaiser@gmail.com>
2021-10-09 16:16:59 +0200

    update to 5.7

--- resources/views/_partials/nav.blade.php
+++ resources/views/_partials/nav.blade.php
@@ -23,7 +23,7 @@
                         @endforeach
                      */
 
-                    $division_ids = [5, 6, 9, 10];
+                    $division_ids = [5, 6, 9, 10, 3];
 
                     $divisions = HLW\Division::find($division_ids);
                     $divisions->load('seasons');
